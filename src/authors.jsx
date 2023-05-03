285946631a1cd68e4bb432a596a7e11934da74b8
Fidesnoella <fniragena@gmail.com>
2023-05-03 20:08:23 +0200

    feat: add message when no articles found

--- src/authors.jsx
+++ src/authors.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaArrowLeft } from "react-icons/fa"
 import { useEffect } from "react";
 import { setSelectedArticle } from "./features/news";
@@ -14,6 +14,9 @@ export default function authors() {
     const status = useSelector(selectArticlesStatus)
     const { selectedAuthor: publisher } = useSelector(state => state.publisher)
 
+    useEffect(() => {
+        dispatch(fetchArticles(publisher))
+    }, [publisher])
 
     const filteredNews = articles.length > 0 ? articles.filter((article) => article.source.id === publisher) : [];
 
@@ -25,7 +28,7 @@ export default function authors() {
 
     if (status === 'failed') {
         return (
-            <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-3">
+            <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-3 mt-10">
                 {Array(12).fill().map((_, index) => <LatestLoader key={index} />)}
             </div>
         )
@@ -34,17 +37,29 @@ export default function authors() {
     return (
         <div className="mt-10 flex flex-col gap-4">
             <Link to={"/"} className="w-fit flex items-center mx-3 sm:mx-0 gap-2 text-[#6bc5e9] font-medium text-lg hover:underline"><FaArrowLeft />Back to home</Link>
-            <h1 className="py-4 text-xl sm:text-2xl flex mx-4 sm:mx-0 max-w-xs whitespace-normal">All News of {publisher}</h1>
+            <h1 className="py-4 text-xl sm:text-2xl flex mx-4 sm:mx-0 max-w-xs sm:max-w-none whitespace-normal">All News of {publisher}</h1>
             {status === 'loading' ?
                 <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-3">
                     {Array(12).fill().map((_, index) => <LatestLoader key={index} />)}
                 </div>
                 :
-                <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-3">
-                    {filteredNews?.slice(0, 15)?.map(article => (
-                        <LatestNews img={article.urlToImage} title={article.title} text={article.description} date={article.publishedAt?.substring(0, 10)} key={article.url}
-                            handleClick={() => handleClick(article)} />
-                    ))}
+                <div>
+                    {
+                        filteredNews.length === 0 ?
+                            <div>
+                                <h1 className="py-6 text-xl sm:text-2xl flex mx-4 sm:mx-0 max-w-lg">
+                                    No articles from this publisher were found. Please try again later
+                                </h1>
+                                <button className="bg-[#aad6e8] font-semibold py-2 px-6 cursor-pointer text-gray-700 hover:bg-[#7ecceb]" onClick={() => navigate("/")}>Go Back Home</button>
+                            </div>
+                            :
+                            <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-3">
+                                {filteredNews?.slice(0, 15)?.map(article => (
+                                    <LatestNews img={article.urlToImage} title={article.title} text={article.description} date={article.publishedAt?.substring(0, 10)} key={article.url}
+                                        handleClick={() => handleClick(article)} />
+                                ))}
+                            </div>
+                    }
                 </div>
             }
         </div>
